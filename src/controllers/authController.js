1614c7f725f59f0d6372a51fa289ad5080d43e1d
Rafael Pransa <44833804+RafaelPransa@users.noreply.github.com>
2026-05-29 23:53:55 +0700

    feat: fitur login dan jwt authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,7 +2,9 @@ const knex = require('../../knexfile'); // Panggil konfigurasi knex
 // Inisialisasi knex menggunakan konfigurasi developmentnya
 const db = require('knex')(knex.development);
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+// Fungsi register
 exports.register = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -51,6 +53,69 @@ exports.register = async (req, res) => {
     });
   } catch (error) {
     console.error('Error saat register:', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Terjadi kesalahan pada server internal.',
+    });
+  }
+};
+
+// Fungsi Login
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validasi input dasar
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: 'Email dan password wajib diisi!',
+      });
+    }
+
+    // Cari user berdasarkan email di database PostgreSQL
+    const user = await db('users').where({ email }).first();
+
+    // Jika tidak ditemukan, kirim respon salah 401 Unauthorized
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: 'Email atau password yang anda masukkan salah!',
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        success: false,
+        message: 'Email atau password yang anda masukkan salah!',
+      });
+    }
+
+    // Jika sukses, buat JWT Token (berlaku selama 1 hari / 24 jam)
+    // masukkan data ID dan Role ke dalam token agar Frontend bisa memanfaatkannya
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '24h' },
+    );
+
+    // Kirim respon sukses beserta token ke frontend
+    return res.status(200).json({
+      success: true,
+      message: 'Login berhasil. Selamat datang kembali',
+      data: {
+        token,
+        user: {
+          id: user.id,
+          email: user.email,
+          role: user.role,
+        },
+      },
+    });
+  } catch (error) {
+    console.error('Error saat login:', error);
     return res.status(500).json({
       success: false,
       message: 'Terjadi kesalahan pada server internal.',
